80b46e5f343e058159a325605c8c11e0e431dd84
Morris Kelly <morrissgk@gmail.com>
2020-12-01 23:34:47 +0000

    Add project files.

---
+++ VolumeControl/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CSCore.CoreAudioAPI;
+
+namespace VolumeControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        // Enumerate all the descendants of the visual object.
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var button = (RadioButton)sender;
+
+            using (var device = GetDefaultRenderDevice())
+            using (var endpointVolume = AudioEndpointVolume.FromDevice(device))
+            {
+                var volume = endpointVolume.GetMasterVolumeLevelScalar();
+                Console.WriteLine("Volume is {0}", volume);
+
+
+
+                endpointVolume.SetMasterVolumeLevelScalar(float.Parse(button.Content.ToString())/100, Guid.Empty);
+            }
+
+        }
+
+        public static MMDevice GetDefaultRenderDevice()
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                return enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            }
+        }
+
+    }
+}
